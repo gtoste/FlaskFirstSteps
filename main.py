a27e10c6fbdfd6c664344c9256d18a94cb607078
gczyszczon <gczyszczon39@gmail.com>
2022-09-15 11:44:12 +0200

    Add basic routing, render templates

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>witaj</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Witaj {name}'
+
 if __name__=='__main__':
     app.run()
+
